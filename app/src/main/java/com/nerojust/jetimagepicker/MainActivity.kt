a56f068980e4171c602077e8c33f5d88cd02ea70
nerojust <nyerhovwo.adjekughene@raenest.com>
2025-07-07 10:39:20 +0700

    Updating the missing permissions and files.

--- app/src/main/java/com/nerojust/jetimagepicker/MainActivity.kt
+++ app/src/main/java/com/nerojust/jetimagepicker/MainActivity.kt
@@ -5,43 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.nerojust.jetimagepicker.ui.theme.JetImagePickerTheme
+import com.nerojust.jetimagepicker.example.ImagePickerScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetImagePickerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ImagePickerScreen(Modifier.fillMaxSize())
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetImagePickerTheme {
-        Greeting("Android")
     }
 }
