d65d13d3862ae7cc2342927b51c855b4f876131e
nerojust <nyerhovwo.adjekughene@raenest.com>
2026-09-13 15:36:12 +0100

    build: add media3-transformer dependency for video compression

--- JetImagePicker/build.gradle.kts
+++ JetImagePicker/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.easycrop)
+    implementation(libs.media3.transformer)
 }
 
 detekt {
